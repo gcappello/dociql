3cb1a208313b6ea3b05740cab32d96ae85bb9a27
Jose Luis Cerrada <jl.cerrada@promofarma.com>
2021-02-25 10:15:55 +0100

    Infer example types

--- app/helpers/exampleFromDescription.js
+++ app/helpers/exampleFromDescription.js
@@ -7,7 +7,17 @@ module.exports = function(description) {
     let example = null;
     if (description !== null) {
         const match = description.match(/@example:?[\s]*([^\n]*)/);
-        example = match !== null ? match[1] : example;
+        if (match !== null) {
+            try {
+                example = JSON.parse(String(match[1]));
+            } catch (e) {
+                console.log(
+                    '\x1b[43m\x1b[30m[WARNING]',
+                    `\`${match[1]}\` is not a valid example. If it's a string, make sure it is surrounded by quotation marks \`"\`.`,
+                    '\x1b[0m'
+                )
+            }
+        }
     }
 
     return example;
